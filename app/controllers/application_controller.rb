c340f0f659e737ecb35c6b2747ed95e644d37db6
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-07-06 12:14:39 +0300

    feat: sidebar + add icons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_store, if: :current_user
+
+  private
+
+  def set_store
+    if current_user.store_admin?
+      @store = current_user.store
+    end
+  end
 end
