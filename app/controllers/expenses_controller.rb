4803a95a0da9f6877eaf829f76195194bee372b1
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-09-10 18:41:48 +0300

    feat: expense edit

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,6 +1,12 @@
 class ExpensesController < ApplicationController
 
   before_action :set_day, only: %i[create]
+  before_action :set_expense, except: %i[index create]
+
+  def index
+    @current_month = params[:month] ? Date.parse(params[:month]) : Date.current.beginning_of_month
+    @expenses = @store.expenses.where(date: @current_month..@current_month.end_of_month).group_by { |expense| expense.date.to_date }
+  end
 
   def create
     @expense = @store.expenses.new(expense_params)
@@ -16,10 +22,24 @@ class ExpensesController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      if @expense.update(expense_params)
+        format.html { redirect_to day_path(@expense.day.date), notice: "Expense was successfully updated."}
+      else
+        format.html { render :edit, status: :unprocessable_entity}
+      end
+    end
+  end
+
   private
 
   def expense_params
     params.require(:expense).permit(:amount, :date, :expense_item_id)
+  end
+
+  def set_expense
+    @expense = @store.expenses.find(params[:id])
   end
 
   def set_day
--- app/helpers/store_helper.rb
+++ app/helpers/store_helper.rb
@@ -20,6 +20,16 @@ module StoreHelper
         path: expenses_path
       },
       {
+        title: "#{@current_month_name} Kickback (#{@currency})",
+        data: "#{currency_format @current_month_stock_kickback.to_i}",
+        icon: 'fa-solid fa-rotate'
+      },
+      {
+        title: "#{@current_month_name} Profit (#{@currency})",
+        data: "#{currency_format @current_month_profit.to_i}",
+        icon: 'fa-solid fa-arrow-trend-up'
+      },
+      {
         title: 'Products',
         data: @store.products.size,
         icon: 'fa-solid fa-list',
